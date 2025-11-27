b8cb759eb8e45fc8e4b0cca618fb66786f79f3b2
aimahusnain <aimahusnain@gmail.com>
2025-11-28 00:03:19 +0500

    fdas

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -215,8 +215,8 @@ export default function DashboardAnalysisPage() {
   ]
 
   const chartData = Object.values(dataByModel)
-  const soldCount = filteredData.filter((item: any) => item.sellStatus).length
-  const unsoldCount = filteredData.length - soldCount
+  // const soldCount = filteredData.filter((item: any) => item.sellStatus).length
+  // const unsoldCount = filteredData.length - soldCount
   // const statusData = [
   //   { name: "Sold", value: soldCount },
   //   { name: "Unsold", value: unsoldCount },
